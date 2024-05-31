d46d83170a578e35023f49b943eb1268e056bd9d
majid <alinejad.mj@gmail.com>
2024-05-31 23:13:06 +0330

    update route.ts

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -1,7 +1,6 @@
-import { NextApiRequest } from 'next';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function POST(req: NextApiRequest) {
+export async function POST(req: NextRequest) {
   const data = {
     url: '/checkout',
   };
